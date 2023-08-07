4d7c40b6f3dbd39def2a5f6f1ea2fe8af597aaba
GB_surface <none>
2023-08-07 22:30:31 +0200

    work on settingshandler

--- actUserDataHandling.py
+++ actUserDataHandling.py
@@ -1,4 +1,8 @@
 from gbMongoDB import getUserData, createUserEntry, updateUserEntry
+import os.path
+import pickle
+from typing import Any, Dict, TypeVar
+KeyType = TypeVar('KeyType')
 
 def AUD_addUserData(db, actUserData, chatId, **dataEntries):
     userData = getUserData(db, chatId)
@@ -9,4 +13,31 @@ def AUD_addUserData(db, actUserData, chatId, **dataEntries):
             if dataKey not in userData or dataEntry != userData[dataKey]:
                 updateUserEntry(db, chatId, **{dataKey: dataEntry})
     userDataNew = getUserData(db, chatId)
-    return userDataNew
+    actUserDataStored = getUserDataPickle()
+    actUserData = deep_update_pydantic(actUserData, actUserDataStored)
+    actUserData[chatId] = deep_update_pydantic(actUserData[chatId], userDataNew)
+    saveUserDataPickle(actUserData)
+    return actUserData
+
+def saveUserDataPickle(actUserData):
+    pickle.dump(actUserData, open("actUserData", "wb"))
+def getUserDataPickle():
+    if os.path.isfile("actUserData"):
+        actUserData = pickle.load(open("actUserData", "rb"))
+    else:
+        actUserData = {}
+    return actUserData
+
+def getUserData(chatId):
+    actUserData = getUserDataPickle()
+    return actUserData[chatId]
+
+def deep_update_pydantic(mapping: Dict[KeyType, Any], *updating_mappings: Dict[KeyType, Any]) -> Dict[KeyType, Any]:
+    updated_mapping = mapping.copy()
+    for updating_mapping in updating_mappings:
+        for k, v in updating_mapping.items():
+            if k in updated_mapping and isinstance(updated_mapping[k], dict) and isinstance(v, dict):
+                updated_mapping[k] = deep_update_pydantic(updated_mapping[k], v)
+            else:
+                updated_mapping[k] = v
+    return updated_mapping
---
+++ addenviron.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DBCONNCTSTR"] = "mongodb+srv://telegramuser:Georg123@phgabcluster.krx8kmz.mongodb.net/?retryWrites=true&w=majority"
+os.environ["CRON_JOB_KEY"] = "a15sGCHOZVtvSAGjTG+0eFDUMfLhna/QyzIWi9LpFeo="
+
